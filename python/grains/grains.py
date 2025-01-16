da456414444a5f180554de7a682d7d6b6561ef44
mateoviscarra <mateoviscarrar@gmail.com>
2025-01-16 14:56:21 -0600

    Finished the grains problem.

--- python/grains/grains.py
+++ python/grains/grains.py
@@ -1,6 +1,8 @@
 def square(number):
-    pass
-
-
+    if 0 < number < 65:
+        return 2**(number - 1)
+    else:
+        raise ValueError("square must be between 1 and 64")
+    
 def total():
-    pass
+    return 2**64 - 1
