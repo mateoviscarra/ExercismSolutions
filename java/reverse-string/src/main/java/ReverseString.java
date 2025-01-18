20d40b279da1433eb2952347d099141597cd3a2c
mateoviscarra <mateoviscarrar@gmail.com>
2025-01-18 13:01:00 -0600

    Finished reverse string problem.

--- java/reverse-string/src/main/java/ReverseString.java
+++ java/reverse-string/src/main/java/ReverseString.java
@@ -1,7 +1,13 @@
 class ReverseString {
 
     String reverse(String inputString) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        
+        String reversedString = "";
+
+        for (int i = 0; i < inputString.length(); i++) {
+            reversedString = inputString.charAt(i) + reversedString;
+        }
+
+        return reversedString;
     }
-  
 }
